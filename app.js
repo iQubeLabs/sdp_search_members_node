be3aedecd5567632a4247d59465f02671ad011f9
perfectmak <damiperfect@gmail.com>
2016-06-09 11:22:42 +0100

    fix config module import

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 const process = require('process');
 const express = require('express');
 const loadRoutes = require('./routes/load');
-const config = require('config');
+const config = require('./config');
 
-const appPort = config.APP_PORT;
+const appPort = config.appPort;
 const expressApp = express();
 
 loadRoutes(expressApp);
